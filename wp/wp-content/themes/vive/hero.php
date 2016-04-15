c61f3dfee535bdc85bf0dc773277ed366b901777
stanisger <gersigner@hotmail.com>
2016-04-15 17:52:36 -0500

    integrate our anf hero to wp

--- wp/wp-content/themes/vive/hero.php
+++ wp/wp-content/themes/vive/hero.php
@@ -8,10 +8,15 @@
       $descript = get_sub_field('descript');
       $imagen= get_sub_field('imagen');
       $eyecatcher= get_sub_field('eyecatcher');
-
+      $link= get_sub_field('link');
       ?>
         <div class="section no-pad-bot" id="index-banner" 
-             style="background-image: url(<?php the_field('imagen'); ?>);">
+             style="background-image: url(
+                  <?php 
+                    if( !empty($imagen) ): ?>
+                       <img src='<?php echo $imagen['url']; ?>" alt="<?php echo $imagen['alt']; ?>'/>
+                  <?php endif; ?>
+                  );">
                 <div class="container">
                     <h1 class="text_h center header cd-headline letters type">
                         <span><?php echo $title; ?> </span> 
@@ -20,8 +25,12 @@
                         <?php while( have_rows('word') ): the_row(); 
                                // vars producto
                               $word = get_sub_field('word');
+                              $word2 = get_sub_field('word2');
+                              $word3 = get_sub_field('word3');
                               ?>
-                                 <b class="is-visible"><?php echo $word; ?> </b>
+                                <b class="is-visible"><?php echo $word; ?> </b>
+                                <b><?php echo $word2; ?> </b>
+                                <b><?php echo $word3; ?> </b>
                             <?php endwhile; else: ?>
                             <h1>No se encontraron Articulos</h1>
                             <?php endif; ?>
@@ -31,9 +40,9 @@
                     <a href="<?php echo $link; ?>" class="center">Ver más</a>   
                 </div>
         </div>
-    <?php endwhile; else: ?>
-    <h1>No se encontraron Articulos</h1>
-    <?php endif; ?>
+      <?php endwhile; else: ?>
+      <h1>No se encontraron Articulos</h1>
+      <?php endif; ?>
 <?php endwhile; else: ?>
 <h1>No se encontraron Articulos</h1>
 <?php endif; ?>
--- wp/wp-content/themes/vive/our.php
+++ wp/wp-content/themes/vive/our.php
@@ -1,35 +1,41 @@
-<!--Intro and service-->
-<div id="intro" class="section scrollspy">
-    <div class="container">
-        <div class="row">
-            <div  class="col s12">
-
-                <h2 class="center header text_h2"> Ministerio Vive A.C. es una Asociación Civil fundada por personas comprometidas<span class="span_h2"> con los niños,</span>adultos mujeres y hombres de <span class="span_h2">colonias marginadas.</span> </h2>
-            </div>
-
-            <div  class="col s12 m4 l4">
-                <div class="center promo promo-example">
-                    <i class="mdi-social-group"></i>
-                    <h5 class="promo-caption">Servicio Social</h5>
-                    <p class="light center">240 horas días sábados, todas las Licenciaturas</p>
-
-                </div>
-            </div>
-            <div class="col s12 m4 l4">
-                <div class="center promo promo-example">
-                    <i class="mdi-image-color-lens"></i>
-                    <h5 class="promo-caption">Talleres y cursos</h5>
-                    <p class="light center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-
-                </div>
-            </div>
-            <div class="col s12 m4 l4">
-                <div class="center promo promo-example">
-                    <i class="mdi-action-favorite"></i>
-                    <h5 class="promo-caption">Donaciones</h5>
-                    <p class="light center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-                </div>
+   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>  
+      <!--Intro and service-->
+        <div id="intro" class="section scrollspy">
+            <div class="container">
+                <div class="row">
+                    <div  class="col s12">
+                        <?php if( have_rows('our') ): ?>
+                        <?php while( have_rows('our') ): the_row(); 
+                        // vars producto
+                        $descript = get_sub_field('descript');
+                        ?>
+                        <div class="center header text_h2">
+                           <?php echo $descript; ?>
+                      </div>
+                    </div>
+                    <?php if( have_rows('service') ): ?>
+                    <?php while( have_rows('service') ): the_row(); 
+                    // vars producto
+                     $icon = get_sub_field('icon');
+                     $title = get_sub_field('title');
+                     $descript = get_sub_field('descript');
+                      ?>
+                        <div  class="col s12 m4 l4">
+                            <div class="center promo promo-example">
+                                <i class="mdi-social-group"></i>
+                                <h5 class="promo-caption">Servicio Social</h5>
+                                <p class="light center">240 horas días sábados, todas las Licenciaturas</p>
+                            </div>
+                        </div>
+                     <?php endwhile; else: ?>
+                    <h1>No se encontraron Articulos</h1>
+                    <?php endif; ?>        
+                   <?php endwhile; else: ?>
+                   <h1>No se encontraron Articulos</h1>
+                   <?php endif; ?>   
+               </div>
             </div>
         </div>
-    </div>
-</div>
+<?php endwhile; else: ?>
+<h1>No se encontraron Articulos</h1>
+<?php endif; ?>        
